0647a61ad5cf0009827d94605c04aaf100ffbb49
Chris <chrisvolar@gmail.com>
2022-05-25 14:33:46 -0500

    changes

--- deaf_grandma.js
+++ deaf_grandma.js
@@ -1,5 +1,16 @@
 function deafGrandma() {
+// const prompt = require("prompt-sync")();
+// var input = prompt("HEY KID!\n");
+// console.log(input)
+// if(input === input.toLowerCase) {
+//     console.log("SPEAK UP, KID!");
+// } else if(input === input.toUpperCase ) {
+//     console.log("NO, NOT SINCE 1946!");
+// }
+  const name = window.prompt('what is your name?');
+  document.write("My name is " + name)
 
+    
 }
 
 deafGrandma();
